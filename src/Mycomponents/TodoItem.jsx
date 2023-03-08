7226582391a6bb66dc2dfb1e627118e310de9a09
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-08 23:22:02 +0530

    Passing  todo Through props and destructuring

--- src/Mycomponents/TodoItem.jsx
+++ src/Mycomponents/TodoItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const TodoItem = ({todo}) => {
+const TodoItem = ({todo, onDelete}) => {
   return (
     <div className="m-5 mb-1 mt-2">
       <h4>{todo.title}</h4>
       <h5>{todo.desc}</h5>
-      <button className="btn btn-danger">Delete</button>
+      <button className="btn btn-danger" onClick={() => {onDelete(todo)}} >Delete</button>
     </div>
   )
 }
