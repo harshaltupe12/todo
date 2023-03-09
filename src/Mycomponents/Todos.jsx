97018f0e587fea1d44a0193a8e9a32b99bb19762
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-09 22:42:05 +0530

    Add task 0 Condition

--- src/Mycomponents/Todos.jsx
+++ src/Mycomponents/Todos.jsx
@@ -5,7 +5,7 @@ const Todos = (props) => {
   return (
     <div>
     <h2 className='text-center'>Todo List</h2>
-  {
+  { props.todos.length===0 ? "No Todo To Display Chala Jaa BSDK":
     props.todos.map((todo) =>{
      return <TodoItem todo = {todo} key={todo.sno} onDelete = {props.onDelete}/>
     })
