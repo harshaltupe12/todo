12a1e53a34d7b15f02b18ad6dc6da308aa5890f6
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-10 22:52:40 +0530

    Basic Formating Done in Todos

--- src/Mycomponents/Todos.jsx
+++ src/Mycomponents/Todos.jsx
@@ -4,7 +4,7 @@ import TodoItem  from "./TodoItem";
 const Todos = (props) => {
   return (
     <div>
-    <h2 className='text-center'>Todo List</h2>
+    <h1 className='text-center m-4'>Todo List</h1>
   { props.todos.length===0 ? "No Todo To Display Chala Jaa BSDK":
     props.todos.map((todo) =>{
      return <TodoItem todo = {todo} key={todo.sno} onDelete = {props.onDelete}/>
