c204168e327c68a2404ab6120c9072b54ba11272
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-10 22:52:21 +0530

    NavBar 2.0

--- src/Mycomponents/Navbar.jsx
+++ src/Mycomponents/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Navbar(props) {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="bg-dark text-light">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand">{props.title}</a>
+          <a className="navbar-brand text-light">{props.title}</a>
           <button
             className="navbar-toggler"
             type="button"
@@ -15,17 +15,17 @@ export default function Navbar(props) {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon text-light" ></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page">
+                <a className="nav-link active text-light" aria-current="page">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link">About</a>
+                <a className="nav-link text-light">About</a>
               </li>
             </ul>
           </div>
