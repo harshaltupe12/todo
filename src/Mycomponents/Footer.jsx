e5d3b48916fa0661a23324649c10a1a7203e17bb
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-10 22:52:06 +0530

    Create a Footer

--- src/Mycomponents/Footer.jsx
+++ src/Mycomponents/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Footer = () => {
+  let myStyle = {
+    position: 'absolute',
+    top: '100vh',
+    width: '100%'
+  } 
   return (
     <div>
-      fotter also works fine
+      <div className="bg-dark text-light" style={myStyle}>
+      <p className="text-center p-3">
+      Copy Right 2023 &copy; Reserved | MyTodo.com
+      </p>
+      </div>
     </div>
   )
 }
