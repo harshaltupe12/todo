0fb90b341d7beb6c900c7eb61dec1458c3c03181
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-11 20:11:45 +0530

    Footer Was Created With New Styling

--- src/Mycomponents/Footer.jsx
+++ src/Mycomponents/Footer.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
 const Footer = () => {
+
   let myStyle = {
-    position: 'absolute',
-    top: '100vh',
-    width: '100%'
+    position: "sticky",
+    left: "0",
+    bottom: "0",
+    width:"100%",
+    textAlign: "center",
+    padding: "10px",
+    marginTop: "30px"
   } 
+
   return (
     <div>
       <div className="bg-dark text-light" style={myStyle}>
       <p className="text-center p-3">
       Copy Right 2023 &copy; Reserved | MyTodo.com
+      <p>
+      Create With ❤️ by Harshal Tupe
+      </p>
       </p>
       </div>
     </div>
