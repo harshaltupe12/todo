6f6a5c8457c9af66b7e160311639496ca534594b
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-11 20:12:10 +0530

    Todo Properties Change

--- src/Mycomponents/Todos.jsx
+++ src/Mycomponents/Todos.jsx
@@ -4,12 +4,12 @@ import TodoItem  from "./TodoItem";
 const Todos = (props) => {
   return (
     <div>
-    <h1 className='text-center m-4'>Todo List</h1>
-  { props.todos.length===0 ? "No Todo To Display Chala Jaa BSDK":
-    props.todos.map((todo) =>{
-     return <TodoItem todo = {todo} key={todo.sno} onDelete = {props.onDelete}/>
-    })
-  }
+      <h1 className='m-4'>Todo List</h1>
+      { props.todos.length===0 ? "No Todo To Display Chala Jaa BSDK":
+        props.todos.map((todo) =>{
+        return <TodoItem todo = {todo} key={todo.sno} onDelete = {props.onDelete}/>
+        })
+      }
     </div>
   )
 }
