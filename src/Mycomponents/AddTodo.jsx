f9ed1f4b8bde3696bc937cdcddfac1e0084e4c69
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-12 23:48:53 +0530

    Main Functionality of adding todo Done

--- src/Mycomponents/AddTodo.jsx
+++ src/Mycomponents/AddTodo.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useState } from "react"
+import React  from 'react'
 
 export const AddTodo = () => {
     let FormStyle = {
@@ -6,21 +7,30 @@ export const AddTodo = () => {
         marginLeft : "38px",
         marginRight: "30px"
     }
+
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    let submit = (e) => {
+        e.preventDefault(); // this is use to stop page reloading
+        if(!title || !desc){
+            alert("Title And Description cannot be blank");
+        }
+
+    }
   return (
     <div>
-
-        <form style={FormStyle}>
-        <div className="FstBlock my-2">
-            <h3>Add Todo</h3>
-            <label htmlFor="Title" className="form-label">Todo Title</label>
-            <input type="text" className="form-control" id="Title" aria-describedby="emailHelp"/>
-        </div>
-        <div className="SndBlock my-2">
-            <label htmlFor="Desc" className="form-label">Todo Description</label>
-            <input type="text" className="form-control" id="Desc"/>
-        </div>
-        
-        <button type="submit" className=" Main_Btn btn btn-primary m-3">Submit</button>
+        <form onSubmit={submit} style={FormStyle}>
+            <div className="FstBlock my-2">
+                <h3>Add Todo</h3>
+                <label htmlFor="Title" className="form-label">Todo Title</label>
+                <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" id="Title" aria-describedby="emailHelp"/>
+            </div>
+            <div className="SndBlock my-2">
+                <label htmlFor="Desc" className="form-label">Todo Description</label>
+                <input type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} className="form-control" id="Desc"/>
+            </div>
+            <button type="submit" className=" Main_Btn btn btn-primary m-3">Submit</button>
         </form>
 
     </div>
