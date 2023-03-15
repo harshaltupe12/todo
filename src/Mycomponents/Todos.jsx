90033890ee109be47419a3499af38e3bd49d3345
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-15 23:10:20 +0530

    BAsic Styling Done in Todos Component

--- src/Mycomponents/Todos.jsx
+++ src/Mycomponents/Todos.jsx
@@ -6,7 +6,12 @@ const Todos = (props) => {
       <h1 className='m-4'>Todo List</h1>
       { props.todos.length===0 ? "No Todo To Display Chala Jaa BSDK":
         props.todos.map((todo) =>{
-        return <TodoItem todo = {todo} key={todo.sno} onDelete = {props.onDelete}/>
+        return( 
+        <>
+        <TodoItem todo = {todo} key={todo.sno} onDelete = {props.onDelete}/>
+        <hr/>
+        </>
+        )
         })
       }
     </div>
